7bdc0eb06bb01cfd98f1f8ce22b1443558b614c7
Rahulkrishnan R <rahul.krishnan@talenq.com>
2024-08-09 12:27:27 +0530

    Update api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -6,15 +6,17 @@ const BASE_URL = process.env.BASE_URL as string;
 export const apiService = { showAllLists, addNewList, updateList, deleteList };
 
 function showAllLists(page: number): Promise<AxiosResponse<ListData[]>> {
-  return axios.get(`https://to-do-list-e5u8.onrender.com/api/v1/?page=${String(page)}`);
+  return axios.get(
+    `https://to-do-list-oznv.onrender.com/api/v1/?page=${String(page)}`
+  );
 }
 
 function addNewList(body: any): Promise<AxiosResponse<Task>> {
-  return axios.post(`https://to-do-list-e5u8.onrender.com/api/v1`, body);
+  return axios.post(`https://to-do-list-oznv.onrender.com/api/v1`, body);
 }
 function updateList(id: string, body: any): Promise<AxiosResponse<Task>> {
-  return axios.put(`https://to-do-list-e5u8.onrender.com/api/v1/${id}`, body);
+  return axios.put(`https://to-do-list-oznv.onrender.com/api/v1/${id}`, body);
 }
 function deleteList(id: string): Promise<AxiosResponse<Task>> {
-  return axios.delete(`https://to-do-list-e5u8.onrender.com/api/v1/${id}`);
+  return axios.delete(`https://to-do-list-oznv.onrender.com/api/v1/${id}`);
 }
